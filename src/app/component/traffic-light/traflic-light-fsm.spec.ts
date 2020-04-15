1b044bdf38cb4b1da29576cec00463345c47042c
w91379137 <w913791379137@gmail.com>
2020-04-15 17:42:48 +0800

    完成 TraflicLightFSM test

--- src/app/component/traffic-light/traflic-light-fsm.spec.ts
+++ src/app/component/traffic-light/traflic-light-fsm.spec.ts
@@ -1,18 +1,51 @@
 import { prettyPrintFSM } from 'src/app/share/fsm-debug';
-import { TraflicLightFSM } from './traflic-light-fsm';
+import { TraflicLightFSM, LightStatus } from './traflic-light-fsm';
 // ng test --main ./src/app/component/traffic-light/traflic-light-fsm.spec.ts
 
-prettyPrintFSM(TraflicLightFSM);
+// prettyPrintFSM(TraflicLightFSM);
+
+function getTraflicLight(state: LightStatus) {
+  const fsm = new TraflicLightFSM();
+  fsm.State = state;
+  fsm.InGreenTime = 999;
+  fsm.InYellowTime = 999;
+  fsm.InRedTime = 999;
+  // fsm.Notice.subscribe(e => {
+  //   e.self = Object.assign({}, e.self);
+  //   console.log(e);
+  // });
+  return fsm;
+}
 
 describe(TraflicLightFSM.name, () => {
 
-  it('isActionPointFull', async (done) => {
-    // const fsm = getRole(RoleStatus.Idle);
+  it('isInGreenTimeFull', async (done) => {
+    const fsm = getTraflicLight(LightStatus.Green);
 
-    // const isDo = fsm.addAction(100);
+    const isDo = fsm.increaseTime();
 
-    // expect(isDo).toBe(true);
-    // expect(fsm.State).toBe(RoleStatus.Ready);
+    expect(isDo).toBe(true);
+    expect(fsm.State).toBe(LightStatus.Yellow);
+    done();
+  });
+
+  it('isInYellowTimeFull', async (done) => {
+    const fsm = getTraflicLight(LightStatus.Yellow);
+
+    const isDo = fsm.increaseTime();
+
+    expect(isDo).toBe(true);
+    expect(fsm.State).toBe(LightStatus.Red);
+    done();
+  });
+
+  it('isInRedTimeFull', async (done) => {
+    const fsm = getTraflicLight(LightStatus.Red);
+
+    const isDo = fsm.increaseTime();
+
+    expect(isDo).toBe(true);
+    expect(fsm.State).toBe(LightStatus.Green);
     done();
   });
 
